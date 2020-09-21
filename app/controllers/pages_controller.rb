f4b742ec0d7fd677bc4da1c455a9b8a954bd0948
Norfa Bagas <bgsnorfa@gmail.com>
2020-09-21 11:42:53 +0700

    Add root route for authenticated user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def authenticated
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'register', to: 'devise/registrations#new'
   end
+
+  authenticated :user do
+    root 'pages#authenticated', to: 'pages#authenticated', as: :authenticated_root
+  end
+
   root 'pages#home'
 end
