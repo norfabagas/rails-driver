51f7571e983b3a49f7cc71b1c6c85ace1ed358d3
Norfa Bagas <bgsnorfa@gmail.com>
2020-09-21 11:37:39 +0700

    Add navigation, flash message to layouts/application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new'
+  end
   root 'pages#home'
 end
